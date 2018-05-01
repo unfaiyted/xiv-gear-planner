a4d5e3464aa8913a650668a607823990ec2d4dcf
unfaiyted <unfaiyted@gmail.com>
2018-05-01 11:09:55 -0500

    first commit

---
+++ src/main/java/com/xiv/gearplanner/GearplannerApplication.java
@@ -0,0 +1,12 @@
+package com.xiv.gearplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearplannerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xiv/gearplanner/GearplannerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiv.gearplanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GearplannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
