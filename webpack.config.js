5c3a254597db9b109cd1cb1f1b8320ed1fa95ad5
unfaiyted <unfaiyted@gmail.com>
2018-05-07 10:44:38 -0500

    began building static interface and data structure models

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+// for resolving the absolute path to our project
+// necessary for webpack
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    // where our app "starts"
+    // add the promise and fetch polyfill first
+    entry: ['promise-polyfill', 'whatwg-fetch', './src/main/resources/static/js/global.js'],
+    // where to put the transpiled javascript
+    output: {
+        path: path.resolve(__dirname, 'src/main/resources/static/built'),
+        filename: 'main.js'
+    },
+
+    // babel config
+    module: {
+        rules: [
+            {
+                // any file that ends with '.js'
+                test: /\.js$/,
+                // except those in "node_modules"
+                exclude: /node_modules/,
+                // transform with babel
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
+
+    // allows us to see how the transpiled js relates to the untranspiled js
+    devtool: 'source-map',
+
+    devServer: {
+        contentBase: path.join(__dirname, 'src/main/resources/static/js'),
+        port: 3143,
+        compress: true,
+        watchContentBase: true,
+        // send requests that start with "/api" to our api server
+        proxy: {
+            '/api': {
+                target: 'http://localhost:9090',
+                pathRewrite: {'^/api': ''}
+            }
+        }
+    }
+};
